5b977418064f692b59aaaa32f89de3302fabc0da
Pierre Bresson <262128333+pielouNW@users.noreply.github.com>
2026-05-26 10:57:29 +0200

    Update InfoView

--- nobodywho/swift/Examples/NobodyWatch/Valkyrie/Screens/InfoView.swift
+++ nobodywho/swift/Examples/NobodyWatch/Valkyrie/Screens/InfoView.swift
@@ -1,20 +1,11 @@
-//
-//  InfoView.swift
-//  NobodyWatch Watch App
-//
-
 import SwiftUI
 
 struct InfoView: View {
     var body: some View {
         List {
-            Text("This app was made by Pierre Bresson, using NobodyWho inference engine. LLMs are downloaded from Hugging Face.")
+            Text("This app is made by NobodyWho.\n\nNobodyWho is an open-source library designed to run LLMs locally and efficiently on any device.\n\nLLMs are downloaded from Hugging Face.")
                 .listRowBackground(Color.clear)
                 .padding(.bottom, 8)
-
-            ShareLink(item: URL(string: "https://pierre.bresson.io/")!) {
-                Label("Pierre Bresson", systemImage: "globe")
-            }
 
             ShareLink(item: URL(string: "https://github.com/nobodywho-ooo/nobodywho")!) {
                 Label("NobodyWho", systemImage: "chevron.left.forwardslash.chevron.right")
@@ -25,5 +16,11 @@ struct InfoView: View {
             }
         }
         .navigationTitle("Info")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        InfoView()
     }
 }
